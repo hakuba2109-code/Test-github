5526a6eeadfaad9a7587c4a1722e1756efae95c5
hakuba2109-code <hakuba2109@gmail.com>
2025-09-16 15:19:33 +0700

    Add project files.

---
+++ Test github/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_github
+{
+    public class Class1
+    {
+        private string hoTen;
+        private int tuoi;
+        private string diaChi;
+        public Class1(string hoTen, int tuoi, string diaChi)
+        {
+            this.hoTen = hoTen;
+            this.tuoi = tuoi;
+            this.diaChi = diaChi;
+        }
+        public void InThongTin()
+        {
+            Console.WriteLine("Họ tên: " + hoTen);
+            Console.WriteLine("Tuổi: " + tuoi);
+            Console.WriteLine("Địa chỉ: " + diaChi);
+        }
+    }
+}
---
+++ Test github/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_github
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 person = new Class1("Nguyễn Văn A", 25, "Hà Nội");
+            person.InThongTin();
+            Console.ReadLine();
+        }
+    }
+}
